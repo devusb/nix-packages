b720f79b5bfe3cf58ed65c61006de1932720cfe1
Morgan Helton <mhelton@gmail.com>
2024-02-11 13:19:45 -0600

    switch to flake-parts, add hercules-ci

--- flake.nix
+++ flake.nix
@@ -3,30 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    hercules-ci-effects.url = "github:mlabs-haskell/hercules-ci-effects/push-cache-effect";
+    attic = {
+      url = "github:zhaofengli/attic";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, hercules-ci-effects, attic }:
     let
-      inherit (nixpkgs.lib) genAttrs;
-      forAllSystems = genAttrs [
+      ciSystems = [ "x86_64-linux" "aarch64-linux" ];
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        hercules-ci-effects.push-cache-effect
+        hercules-ci-effects.flakeModule
+      ];
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
-    in
-    {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-
-      packages = forAllSystems (system:
-        import ./pkgs { pkgs = import nixpkgs { inherit system; }; }
-      );
-
-      overlays = {
-        default = final: prev: import ./pkgs { pkgs = prev; };
+      perSystem = { pkgs, ... }: {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = import ./pkgs { inherit pkgs; };
       };
+      herculesCI.ciSystems = ciSystems;
+      push-cache-effect = {
+        enable = true;
+        attic-client-pkg = attic.packages.x86_64-linux.attic-client;
+        caches.r2d2 = {
+          type = "attic";
+          secretName = "attic";
+          packages = builtins.concatLists (builtins.map (attr: (builtins.attrValues self.packages."${attr}")) ciSystems);
+        };
+      };
+      flake = {
+        overlays = {
+          default = final: prev: import ./pkgs { pkgs = prev; };
+        };
 
-      nixosModules = import ./modules;
-
+        nixosModules = import ./modules;
+      };
     };
 }
