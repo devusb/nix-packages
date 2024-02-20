7c3ef8cbfbbd384029cbaaf174b8f35f782a5399
Morgan Helton <mhelton@gmail.com>
2024-02-19 20:41:06 -0600

    node-red: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }: {
-  node-red = pkgs.callPackage ./node-red { };
   go-simple-upload-server = pkgs.callPackage ./go-simple-upload-server { };
   wolweb = pkgs.callPackage ./wolweb { };
   jellyplex-watched = pkgs.callPackage ./jellyplex-watched { };
