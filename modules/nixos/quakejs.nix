c4bb679cb8f3990d230681f164e4fffdc8d0b0e8
Morgan Helton <mhelton@gmail.com>
2024-02-24 16:57:52 -0600

    add quakejs dedicated server module

--- modules/nixos/quakejs.nix
+++ modules/nixos/quakejs.nix
@@ -1,36 +1,107 @@
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) mkEnableOption mkPackageOption mkOption types mkIf getExe;
+  inherit (lib) mkEnableOption mkPackageOption mkOption types mkIf getExe getExe';
   cfg = config.services.quakejs;
 in
 {
   options.services.quakejs = with types; {
-    enable = mkEnableOption (mdDoc "Web ioquake3 client");
     package = mkPackageOption pkgs "quakejs" { };
-    settings = mkOption {
-      type = types.attrsOf types.unspecified;
-      default = {
-        content = "content.quakejs.com";
+    user = mkOption {
+      type = types.str;
+      default = "quakejs";
+      description = lib.mdDoc "User account under which quakejs runs";
+    };
+    group = mkOption {
+      type = types.str;
+      default = "nogroup";
+      description = lib.mdDoc "Group under which quakejs runs";
+    };
+    client = mkOption {
+      type = types.submodule {
+        options = {
+          enable = mkEnableOption (mdDoc "quakejs web client");
+          settings = mkOption {
+            type = types.attrsOf types.unspecified;
+            default = {
+              content = "content.quakejs.com";
+            };
+          };
+        };
       };
+      default = { };
+    };
+    server = mkOption {
+      type = types.submodule {
+        options = {
+          enable = mkEnableOption (mdDoc "quakejs dedicated server");
+          dataDir = mkOption {
+            type = types.str;
+            default = "/var/lib/quakejs";
+            description = "The directory where quakejs stores its files";
+          };
+          extraConfig = mkOption {
+            type = types.lines;
+            default = "";
+            description = (mdDoc "Extra configuration options");
+          };
+        };
+      };
+      default = { };
     };
   };
 
   config =
     let
-      configFile = pkgs.writeTextFile {
+      clientConfigFile = pkgs.writeTextFile {
         name = "web.json";
-        text = builtins.toJSON cfg.settings;
+        text = builtins.toJSON cfg.client.settings;
       };
+      serverConfigFile = pkgs.writeTextFile {
+        name = "q3ds.cfg";
+        text = cfg.server.extraConfig;
+      };
+      serverConfigFilePath = "${cfg.server.dataDir}/base/baseq3/${serverConfigFile.name}";
+      serverInit = pkgs.writeShellScriptBin "server-init.sh" ''
+        cp ${serverConfigFile} ${serverConfigFilePath} 
+        chown ${cfg.user}:${cfg.group} ${serverConfigFilePath} 
+      '';
     in
-    mkIf cfg.enable {
-      systemd.services.quakejs = {
-        description = "quakejs";
+    mkIf (cfg.server.enable || cfg.client.enable) {
+      users.users = mkIf (cfg.user == "quakejs") {
+        quakejs = { group = cfg.group; isSystemUser = true; };
+      };
+
+      systemd.services.quakejs-client = mkIf (cfg.client.enable == true) {
+        description = "quakejs client";
         after = [ "network.target" ];
         wantedBy = [ "multi-user.target" ];
 
         serviceConfig = {
-          ExecStart = "${getExe cfg.package} --config ${configFile}";
-          DynamicUser = true;
+          User = cfg.user;
+          Group = cfg.group;
+          ExecStart = "${getExe' cfg.package "quakejs"} --config ${clientConfigFile}";
+          Restart = "on-failure";
+        };
+      };
+
+      systemd.tmpfiles.settings."quakejs-data"."${cfg.server.dataDir}/base/baseq3".d = mkIf (cfg.server.enable == true) {
+        user = cfg.user;
+        group = cfg.group;
+      };
+      systemd.services.quakejs-server = mkIf (cfg.server.enable == true) {
+        description = "quakejs server";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          User = cfg.user;
+          Group = cfg.group;
+          WorkingDirectory = cfg.server.dataDir;
+          ReadOnlyPaths = serverConfigFilePath;
+
+          ExecStartPre = "${getExe serverInit}";
+          ExecStart = "${getExe' cfg.package "ioq3ded"} +exec ${serverConfigFile.name}";
+          Restart = "on-failure";
         };
       };
     };
--- pkgs/quakejs/default.nix
+++ pkgs/quakejs/default.nix
@@ -3,6 +3,14 @@
 , fetchFromGitHub
 , nodejs
 }:
+let
+  quakejs-docker = fetchFromGitHub {
+    owner = "treyyoder";
+    repo = "quakejs-docker";
+    rev = "6cbe1c4a0024505dc4f00f524b9ecbecd0d2d0c0";
+    hash = "sha256-62CaI9+ivOTm8rjGl5t1ZcFRTA6ZIXLqm3pImMS7mNU=";
+  };
+in
 buildNpmPackage {
   name = "quakejs";
 
@@ -17,12 +25,12 @@ buildNpmPackage {
 
   postPatch = ''
     cp ${./package-lock.json} ./package-lock.json
+    cp ${quakejs-docker}/include/ioq3ded/ioq3ded.fixed.js ./build/ioq3ded.js
   '';
 
   dontNpmBuild = true;
 
   postInstall = ''
-    rm $out/bin/*
     makeWrapper ${lib.getExe nodejs} $out/bin/quakejs --add-flags "$out/lib/node_modules/quakejs/bin/web.js" 
     makeWrapper ${lib.getExe nodejs} $out/bin/ioq3ded --add-flags "$out/lib/node_modules/quakejs/build/ioq3ded.js"
   '';
