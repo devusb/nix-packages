1109cf95dd957ce681d116a65b44f2024dc37e8c
Morgan Helton <mhelton@gmail.com>
2025-04-10 06:10:48 -0500

    wolweb: 1-unstable-2024-10-17 -> 1-unstable-2025-04-09

--- pkgs/wolweb/default.nix
+++ pkgs/wolweb/default.nix
@@ -5,23 +5,16 @@
   fetchpatch,
 }:
 
-buildGoModule rec {
+buildGoModule {
   pname = "wolweb";
-  version = "1-unstable-2024-10-17";
+  version = "1-unstable-2025-04-09";
 
   src = fetchFromGitHub {
     owner = "sameerdhoot";
     repo = "wolweb";
-    rev = "b447d507c4cc7068116946c2981cafcf130cecfa";
-    hash = "sha256-y9CzO4o+LZeAappk/KGX2vP2TtrT5Rp3OxI6ncCEXn0=";
+    rev = "13cdb99a84159db775b23ed43eac1d5901681044";
+    hash = "sha256-xSG65qh+gr2WenRZShsgV/VkpO00c2VkrZHP5IgHesI=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/sameerdhoot/wolweb/pull/36.patch";
-      hash = "sha256-xd1OCCO8PqHT032GIzfkgc5a54SrY77rIOTB9d0Mnlk=";
-    })
-  ];
 
   vendorHash = "sha256-bUUZ/R0hPRYVqtrw8yUlVbxpnKoEGEb85saAn+9MFbo=";
 
@@ -34,7 +27,7 @@ buildGoModule rec {
     description = "Web interface for sending Wake-on-lan (magic packet). An HTTP server built using GoLang and uses Bootstrap for UI";
     homepage = "https://github.com/sameerdhoot/wolweb";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ devusb ];
     mainProgram = "wolweb";
   };
 }
