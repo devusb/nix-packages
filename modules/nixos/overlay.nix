e63673a6c9c37fd504cfc72a62cfdf9234dc6a40
Morgan Helton <mhelton@gmail.com>
2025-11-02 22:11:31 -0600

    fix(overlay): system -> stdenv.hostPlatform.system

--- modules/nixos/overlay.nix
+++ modules/nixos/overlay.nix
@@ -3,7 +3,7 @@
   # ensure packages from this flake are overlayed onto the system nixpkgs
   nixpkgs.overlays = [
     (_: prev: import ../../pkgs { pkgs = prev; })
-    (_: prev: { deckbd = inputs.deckbd.packages.${prev.system}.default; })
+    (_: prev: { deckbd = inputs.deckbd.packages.${prev.stdenv.hostPlatform.system}.default; })
   ];
 
 }
